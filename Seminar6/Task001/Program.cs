e935ad4712a933525bd554f6b547f43e68482b32
Sergei <honholoi.serg09@mail.ru>
2023-02-05 18:05:47 +0800

    sem6zad1

---
+++ Seminar6/Task001/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Задача 1: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 */
+int array = 5;
+int count = 0;
+while (array > 0)
+{
+    Console.Write("Введите число ");
+    string num = Console.ReadLine();
+    int number = Convert.ToInt32(num);
+    if (number > 0)
+    {
+        count++;
+        array--;
+    }
+}
+System.Console.WriteLine();
+System.Console.WriteLine($"Количество чисел больше 0 в заданном массиве: {count}");
+
